75122b7ccda19afc47ef74db7bd8ad25f6a51bcc
Mjam <mjam85.jm@gmail.com>
2022-03-02 22:29:27 -0600

    Examen1 base de datos Entidad Parametro

---
+++ BaseDeDatos/dbo/StoreProcedures/ParametroInsertar.sql
@@ -0,0 +1,46 @@
+﻿CREATE PROCEDURE [dbo].[ParametroInsertar]
+	@Id_Parametro INT,
+	@Codigo VARCHAR(250),
+	@Descripcion VARCHAR(250),
+	@Valor VARCHAR(250),
+	@Estado bit
+	
+
+AS BEGIN
+SET NOCOUNT ON
+
+  BEGIN TRANSACTION TRASA 
+    
+	BEGIN TRY
+
+	 INSERT INTO dbo.Parametro
+	 (  Id_Parametro
+		,Codigo
+		,Descripcion
+		,Valor
+		,Estado 
+	 )
+	 VALUES
+	 (  @Id_Parametro
+		,@Codigo
+		,@Descripcion
+		,@Valor
+		,@Estado	 
+	 )
+
+	 COMMIT TRANSACTION TRASA
+	 SELECT 0 AS CodeError, '' AS MsgError
+
+	END TRY
+
+	BEGIN CATCH
+
+	 SELECT 
+	       ERROR_NUMBER() AS CodeError,
+		   ERROR_MESSAGE() AS MsgError
+
+     ROLLBACK TRANSACTION TRASA
+
+	END CATCH
+
+  END
---
+++ BaseDeDatos/dbo/StoreProcedures/ParametroObtener.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[ParametroObtener]
+	@Id_parametro INT = NULL
+
+AS 
+BEGIN
+  SET NOCOUNT ON
+
+  /*Obtener datos de proveedor*/
+  SELECT
+         Id_Parametro
+		,Codigo
+		,Descripcion
+		,Valor
+		,Estado
+  FROM 
+       dbo.Parametro
+  WHERE
+      (@Id_parametro IS NULL OR Id_Parametro=@Id_Parametro)
+
+END
